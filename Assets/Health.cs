1d7152600091dafad2c74088233a8b831882bd71
filipSlesicki <>
2024-11-20 16:09:01 +0100

    Add health bars

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -1,64 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
+    public Slider healthSlider;
+    public Image healthBar;
+    public TMP_Text healthText;
     public float maxHealth = 10;
-    public float currentHealth = 5;
+    private float currentHealth = 5;
     public float regenRate = 1;
-    public bool isOverFullHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         float missingHealth = maxHealth - currentHealth;
         float timeToRegen = missingHealth / regenRate;
         Debug.Log("It will take " + timeToRegen + " seconds to regenerate to full health");
-
+        currentHealth = maxHealth;
+        UpdateHealthUI();
     }
 
     // Update is called once per frame
     void Update()
+    { 
+    }
+
+    public void ModifyHealth(float amount)
     {
-
-
-        isOverFullHealth = currentHealth > maxHealth;
-        bool isDead = currentHealth <= 0;
-        bool isAlive = currentHealth > 0;
-        isAlive = !isDead;
-        bool isOverHalfHealth = currentHealth > maxHealth / 2;
-        bool shouldBeHealed = currentHealth > 0 && currentHealth <= maxHealth;
-        bool shouldNotBeHealed = currentHealth <= 0 || currentHealth >= maxHealth;
-
-        //if (shouldBeHealed)
-        //{
-        //    currentHealth += regenRate * Time.deltaTime;
-        //    if (currentHealth > maxHealth)
-        //    {
-        //        currentHealth = maxHealth;
-        //    }
-        //}
-
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        currentHealth += amount;
+        if(currentHealth > maxHealth)
         {
-            currentHealth += 1;
-            if(currentHealth > maxHealth)
-            {
-                currentHealth = maxHealth;
-            }
+            currentHealth = maxHealth;
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if(currentHealth < 0)
         {
-            currentHealth -= 1;
-            if(currentHealth < 0)
-            {
-                currentHealth = 0;
-            }
+            // Death
         }
+        UpdateHealthUI();
+    }
 
-
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Space was pressed");
-        }
+    private void UpdateHealthUI()
+    {
+        //healthText.text = "Health " + currentHealth + " / " + maxHealth;
+        float normalizedHealth = (float)currentHealth / maxHealth;
+        //healthSlider.maxValue = maxHealth;
+        //healthSlider.value = normalizedHealth;
+        healthBar.fillAmount = normalizedHealth;
     }
 }
--- Assets/TugOfWar.cs
+++ Assets/TugOfWar.cs
@@ -16,29 +16,92 @@ public class TugOfWar : MonoBehaviour
     {
         if (!gameFinished)
         {
-            timer -= Time.deltaTime;
-            if (timer <= 0)
-            {
-                transform.Translate(-speed, 0, 0);
-                timer = timeBetweenMoves;
-            }
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                transform.Translate(speed, 0, 0);
-                DebugTest();
-            }
-            if (transform.position.x >= distanceToWin)
-            {
-                winnerText.text = "You win";
-                gameFinished = true;
-                DebugTest();
-            }
-            if (transform.position.x <= -distanceToWin)
-            {
-                winnerText.text = "You lose";
-                gameFinished = true;
-            }
+            UpdateAIMovement();
+            UpdatePlayerMovement();
+            UpdateGameFinished();
         }
+
+        float sumAB = Add(10, 5);
+        Debug.Log(Add(1, 3.5f));
+        int roundedDownValue = Mathf.FloorToInt(1.5f);
+        Mathf.Max(10, 15);
+        if(!TryMove(1))
+        {
+            Debug.Log("Cant move. Game is finished");
+        }
+    }
+
+    private bool TryMove(int direction)
+    {
+        if(gameFinished)
+        {
+            return false;
+        }
+
+        Move(direction);
+        return true;
+    }
+
+    private float Max(float a, float b)
+    {
+        if(a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+    /// <summary>
+    /// Move object
+    /// </summary>
+    /// <param name="direction">1 is right or -1 is left</param>
+    public void Move(int direction)
+    {
+        if (!gameFinished)
+        {
+            transform.Translate(speed * direction, 0, 0);
+        }
+    }
+
+    private void UpdatePlayerMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(1);
+        }
+    }
+
+    private void UpdateAIMovement()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Move(-1);
+            timer = timeBetweenMoves;
+        }
+    }
+
+    private void UpdateGameFinished()
+    {
+        if (transform.position.x >= distanceToWin)
+        {
+            winnerText.text = "You win";
+            gameFinished = true;
+            DebugTest();
+        }
+        if (transform.position.x <= -distanceToWin)
+        {
+            winnerText.text = "You lose";
+            gameFinished = true;
+        }
+    }
+
+    public float Add(float a, float b)
+    {
+        return a + b;
     }
 
     public void DebugTest()
@@ -46,15 +109,6 @@ public class TugOfWar : MonoBehaviour
         Debug.Log("Test");
         Debug.Log("Test2");
         Debug.Log("Test2");
-
-        Debug.Log("Test3");
-
-
-
-
-
-
-
     }
 
 }
